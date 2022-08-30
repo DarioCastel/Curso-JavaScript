1a9dce1485b418200afe808e2d4acc296a0a6f9e
DarioCastel <lecastelnovo@gmail.com>
2022-08-30 18:50:10 -0300

    reloj finish

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -1,1 +1,37 @@
 /* **********     Curso JavaScript: 82. DOM: Ejercicios Prácticos | Reloj Digital - #jonmircha     ********** */
+const d= document
+export function relojDigital(reloj, inireloj,detreloj){
+    let clockTempo;
+
+    d.addEventListener("click", (e)=> {
+       if (e.target.matches(inireloj)){
+       clockTempo = setInterval(()=> {
+            let clockHour = new Date().toLocaleTimeString();
+            d.querySelector(reloj).innerHTML = `</h3>${clockHour} </h3>`;
+        },1000)
+        e.target.disabled = true;
+    }
+        if(e.target.matches(detreloj)){
+            clearInterval(clockTempo);
+            d.querySelector(reloj).innerHTML = null;
+            d.querySelector(inireloj).disabled = false;
+
+        }
+});
+}
+
+export function alarma(alarma, inialarma,detalarma){
+    let soundTempo;
+    d.addEventListener("click", e=> {
+        if(e.target.matches(inialarma)){
+
+        }
+        if(e.target.matches(detalarma)){
+
+        }
+
+
+    })
+
+
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,8 +1,9 @@
 /* **********     Curso JavaScript: 80. DOM: Ejercicios Prácticos | Presentación - #jonmircha     ********** */
 import hamburguesaMenu from "./menu_hamburguesa.js";
-
+import {relojDigital,alarma} from "./dom/reloj.js";
 const d=document
 
 d.addEventListener("DOMContentLoaded",e =>{
     hamburguesaMenu(".panel-btn",".panel",".menu a")
+    relojDigital("#reloj","#inireloj","#detreloj")
 })
